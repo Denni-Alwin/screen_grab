655316d13775aa482c27281cdf9713872dce48fb
Denni-Alwin <65033982+Denni-Alwin@users.noreply.github.com>
2021-03-20 19:26:51 +0530

    Add files via upload

---
+++ Screen_grab.py
@@ -0,0 +1,14 @@
+import numpy as np
+from PIL import ImageGrab
+import cv2
+
+
+while(True):
+   # 800x600 windowed mode
+   printscreen =  np.array(ImageGrab.grab(bbox=(5,5,1280,720)))
+   cv2.imshow('window',cv2.cvtColor(printscreen, cv2.COLOR_BGR2RGB))
+   if cv2.waitKey(25) & 0xFF == ord('q'):
+            cv2.destroyAllWindows()
+            break
+
+
